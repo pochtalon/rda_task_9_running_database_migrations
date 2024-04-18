7fed1b8eb34c2ba744b93f036508ab08a4e7aab4
pochtalon <pochtalon1010@gmail.com>
2024-04-18 09:24:25 +0300

    Solved

--- task.sql
+++ task.sql
@@ -51,4 +51,4 @@ CREATE TABLE Users (
 
 --changeset pochtalon:6 labels:0.0.3
 CREATE INDEX Email ON Users (Email);
---rollback DROP INDEX Email ON Users (Email);
+--rollback ALTER TABLE Users DROP INDEX Email;
